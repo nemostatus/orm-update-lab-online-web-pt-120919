3bbc35f9b1201befcd96aabfe6306211dc8b5468
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 20:19:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ def self.create_table
 end 
 
 def self.drop_table 
-  DB[:conn].execute("DROP TABLE IF NOT EXISTS")
+  DB[:conn].execute("DROP TABLE IF NOT EXISTS students")
 end 
 def self.create(name:,grade:)
   student = Student.new(name,grade)
