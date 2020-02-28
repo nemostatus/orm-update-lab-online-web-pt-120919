e46442820e2c712d02cdada62c80d7c35ee70a3e
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 20:38:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,11 @@ DB[:conn].execute(sql,self.name,self.grade)
 students ")[0][0]
 end 
 
-
+def self.create(name:, grade:)
+   student = self.new(name, grade)
+   student.save
+   student
+ end
 def self.new_from_db(row)
   id = row[0]
   name = row[1]
@@ -56,10 +60,6 @@ def self.find_by_name(name)
   sql = "SELECT * FROM students WHERE name = ?"
   DB[:conn].execute(sql,name).map {|row| new_from_db(row)}.first
 end 
- def self.create(name:, grade:)
-   student = self.new(name, grade)
-   student.save
-   student
- end
+ 
  
 end 
