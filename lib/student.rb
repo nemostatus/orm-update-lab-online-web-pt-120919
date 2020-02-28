153da28e8124fa5c44eeb592788aefa1e43ae7e1
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 20:35:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,8 @@ def save
   VALUES(?,?)
   SQL
 DB[:conn].execute(sql,self.name,self.grade)
-@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students ")[0][0]
+@id = DB[:conn].execute("SELECT last_insert_rowid() FROM 
+students ")[0][0]
 end 
 
 
