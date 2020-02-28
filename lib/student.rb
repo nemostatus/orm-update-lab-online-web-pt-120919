e63f54038745f85096da347220716ce821294d6e
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 20:24:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ end
 
 def self.find_by_name(name)
   sql = "SELECT * FROM students WHERE name = ?"
-  DB[:conn].execute(sql,name).map {|row| new_from_db(row)}
+  DB[:conn].execute(sql,name).map {|row| new_from_db(row)}.first
   
 def save 
   if self.id 
