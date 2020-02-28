2d660c7f16daf4b9c64dd9fa90aa8f6c0a729798
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 20:33:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,19 @@ def self.create(name:, grade:)
   student
   end 
 
+def save 
+  if self.id 
+    self.update 
+  else 
+  sql = <<-SQL 
+  INSERT INTO students(name,grade)
+  VALUES(?,?)
+  SQL
+DB[:conn].execute(sql,self.name,self.grade)
+@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students ")[0][0]
+end 
+end
+
 def self.new_from_db(row)
   id = row[0]
   name = row[1]
