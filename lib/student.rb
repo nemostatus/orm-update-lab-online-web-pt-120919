e084e842def6f0a9e39ec5d044fdd6463ca523a3
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 20:33:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
 
-attr_accessor :id,:name, :grade
+attr_accessor :name, :grade
+attr_reader :id
 
 
 def initialize(id = nil, name, grade)
