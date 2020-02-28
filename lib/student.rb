05cfbc942d6cde06306334a8d5a7d3f467ef1900
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 20:33:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,17 +44,6 @@ def self.find_by_name(name)
   sql = "SELECT * FROM students WHERE name = ?"
   DB[:conn].execute(sql,name).map {|row| new_from_db(row)}.first
 end 
-def save 
-  if self.id 
-    self.update 
-  else 
-  sql = <<-SQL 
-  INSERT INTO students(name,grade)
-  VALUES(?,?)
-  SQL
-DB[:conn].execute(sql,self.name,self.grade)
-@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students ")[0][0]
-end 
-end 
+ 
  
 end 
